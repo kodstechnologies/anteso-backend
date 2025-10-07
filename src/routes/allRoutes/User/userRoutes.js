d79484dc36ee761791465bd653f51f081bb15fe1
PrajwalaNagekar <prajwalanagekar@gmail.com>
2025-10-07 15:12:36 +0530

    small fix

--- src/routes/allRoutes/User/userRoutes.js
+++ src/routes/allRoutes/User/userRoutes.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
-import { sendOtp, verifyOtp,logout } from '../../../controllers/users/login.controller.js';
+import { sendOtp, verifyOtp, logout } from '../../../controllers/users/login.controller.js';
 import { verifyUserJWT } from '../../../middlewares/userAuthMiddleware.js';
 import { authenticate, refreshAccessToken } from '../../../middlewares/authMiddleware.js';
 import { authorizeRoles } from '../../../middlewares/authorizeRoles.js'
 import machineRoutes from '../../../routes/allRoutes/User/machineRoutes.js'
 const router = Router();
+router.post('/refresh', refreshAccessToken);
 router.use('/send-otp', sendOtp)
 router.use('/verify-otp', verifyOtp)
-router.post('/refresh', refreshAccessToken);
+// router.post('/refresh', refreshAccessToken)
 router.use(authenticate, authorizeRoles("Admin", "Customer", ""))
 // router.post('/machines', machineRoutes)
 router.use('/machines', machineRoutes)
