a4d1d9eb64fe1c8a63a507a0b438c68c40a60ded
prajwala <prajwalanagekar@gmail.com>
2026-03-13 18:06:00 +0530

    added logs for check

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/anteso', mainRouter)
-console.log("calling this after the update")
+console.log("calling this after the update-------------------------------------------")
 app.use(errorHandler);
 const startServer = async () => {
     try {
